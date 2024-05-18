ea34f0b8c731f0eb94c02db7dd409c137d664183
Brayan Martínez <brayananuharmtz11@gmail.com>
2024-05-18 15:22:36 -0600

    Atributos y métodos de la clase AddressEntry creados

--- src/main/java/addres/data/AddressEntry.java
+++ src/main/java/addres/data/AddressEntry.java
@@ -1,5 +1,90 @@
 package addres.data;
 
 public class AddressEntry {
+    // Atributos
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String state;
+    private int zip;
+    private String email;
+    private String phone;
 
+    // Constructor
+    public AddressEntry(String firstName, String lastName, String street, String city, String state, int zip,
+            String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
